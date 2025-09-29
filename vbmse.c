f653892baf7f6ff8e4d487b1b2cba20dece6e7e7
QuiteSkrimple <166445217+QuiteSkrimple@users.noreply.github.com>
2025-09-29 16:39:40 -0700

    Create vbmse.c
    The C code to later be updated to MIPS

---
+++ vbmse.c
@@ -0,0 +1,96 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+#define I 16
+#define J 16
+#define K 4
+#define L 4
+
+int computeSAD(int V[I][J], int B[K][L], int n, int m) {
+    int sum = 0;
+    for (int s = n - K; s < n; s++) {
+        for (int d = m - L; d < m; d++) {
+            sum += abs(V[s][d] - B[s - (n - K)][d - (m - L)]);
+        }
+    }
+    printf("%d, ", sum); //DEBUG
+    return sum;
+}
+
+
+int findBS(int sum, int bs) {
+    if (sum < bs) {
+        bs = sum;
+    }
+    return bs;
+}
+
+int diagonalSearch(int V[I][J], int B[K][L], int *bestN, int *bestM) {
+    int bestSAD = INT_MAX;
+
+    for (int diag = 0; diag <= (I - K) + (J - L); diag++) {
+        if (diag % 2 == 0) { // even diagonals
+            for (int i = 0; i <= diag; i++) {
+                int n = i;
+                int m = diag - i;
+                if (n <= I - K && m <= J - L) {
+                    int sad = computeSAD(V, B, n + K, m + L);
+                    if (sad < bestSAD) {
+                        bestSAD = sad;
+                        *bestN = n;
+                        *bestM = m;
+                    }
+                }
+            }
+        } else { // odd diagonals
+            for (int i = diag; i >= 0; i--) {
+                int n = i;
+                int m = diag - i;
+                if (n <= I - K && m <= J - L) {
+                    int sad = computeSAD(V, B, n + K, m + L);
+                    if (sad < bestSAD) {
+                        bestSAD = sad;
+                        *bestN = n;
+                        *bestM = m;
+                    }
+                }
+            }
+        }
+    }
+    return bestSAD;
+}
+    
+int main() {
+    int window[I][J]={{0, 1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
+                      {1, 2, 3, 4, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,},
+                      {2, 3, 32, 1, 2, 3, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30,},
+                      {3, 4, 1, 2, 3, 4, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45,},
+                      {0, 4, 2, 3, 4, 5, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60,},
+                      {0, 5, 3, 4, 5, 6, 30, 35, 40, 45, 50, 55, 60, 65, 70,  75,},
+                      {0, 6, 12, 18, 24, 30, 36, 42, 48, 54, 60, 66, 72, 78, 84, 90,},
+                      {0, 4, 14, 21, 28, 35, 42, 49, 56, 63, 70, 77, 84, 91, 98, 105,},
+                      {0, 8, 16, 24, 32, 40, 48, 56, 64, 72, 80, 88, 96, 104, 112, 120,},
+                      {0, 9, 18, 27, 36, 45, 54, 63, 72, 81, 90, 99, 108, 117, 126, 135,},
+                      {0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150,},
+                      {0, 11, 22, 33, 44, 55, 66, 77, 88, 99, 110, 121, 132, 143, 154, 165,},
+                      {0, 12, 24, 36, 48, 60, 72, 84, 96, 108, 120, 132, 0, 1, 2, 3,},
+                      {0, 13, 26, 39, 52, 65, 78, 91, 104, 114, 130, 143, 1, 2, 3, 4,},
+                      {0, 14, 28, 42, 56, 70, 84, 98, 112, 126, 140, 154, 2, 3, 4, 5,},
+                      {0, 15, 30, 45, 60, 75, 90, 105, 120, 135, 150, 165, 3, 4, 5, 6}
+    };
+    int frame[K][L]={{0, 1, 2, 3,},
+                     {1, 2, 3, 4,},
+                     {2, 3, 4, 5,},
+                     {3, 4, 5, 6}
+    };
+    
+    int bestX = -1;
+    int bestY = -1;
+    
+    int bestSAD = diagonalSearch(window, frame, &bestX, &bestY);
+    
+    printf("\n\nBest SAD: %d\n", bestSAD); //DEBUG
+    printf("Best SAD Coordinates: [%d, %d]", bestX, bestY);
+    
+    return 0;
+}
